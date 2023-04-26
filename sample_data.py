096e91365d13cca383664727ff0bf77c4709126f
JakubSzym <kubaszym77@gmail.com>
2023-04-26 17:51:12 +0200

    Added some function to vrp.py

--- sample_data.py
+++ sample_data.py
@@ -4,9 +4,8 @@ import random
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("--filename", help="output file")
+parser.add_argument("--filename", "-f", help="output file")
 parser.add_argument("--nodes",    help="number of nodes")
-parser.add_argument("--maxdist",  help="maximal distance for one truck")
 parser.add_argument("--minX",     help="minimal value of x coordinate")
 parser.add_argument("--maxX",     help="maximal value of x coordinate")
 parser.add_argument("--minY",     help="minimal value of y coordinate")
@@ -16,18 +15,14 @@ args = parser.parse_args()
 
 filename = args.filename
 nodes    = int(args.nodes)
-maxdist  = int(args.maxdist)
 minX     = int(args.minX)
 maxX     = int(args.maxX)
 minY     = int(args.minY)
 maxY     = int(args.maxY)
 
 with open(filename, "w") as file:
-  file.write("params:\n")
-  file.write(f"maxdist {maxdist}\n")
-  file.write("nodes:\n")
+  print("label,x,y")
   for i in range(nodes):
-    demand = random.randint(0, maxdist)
     x = random.uniform(minX, maxX)
     y = random.uniform(minY, maxY)
-    file.write(f" N{i+1} {demand} {x} {y}\n")
+    file.write(f"N{i+1},{x},{y}\n")
--- vrp.py
+++ vrp.py
@@ -7,3 +7,37 @@
 #############################################
 
 
+from argparse import ArgumentParser
+import csv
+from platform import node
+import numpy as np
+
+class Node:
+  def __init__(self, label,x, y) -> None:
+    self.label = label
+    self.x = x
+    self.y = y
+      
+class GraphVrp:
+  def __init__(self, maxdist, nodes) -> None:
+    self.maxdist = maxdist
+    self.nodes   = nodes
+
+def process_input_data(filename, maxdist):
+  with open(filename, "r") as file:
+    reader = csv.DictReader(file)
+    nodes = []
+    for row in reader:
+      nodes.append(Node(row['label'], float(row['x']), float(row['y'])))
+    
+  return GraphVrp(maxdist, nodes)
+
+def distance(node1, node2):
+  return np.sqrt((node1.x - node2.x)**2 + (node1.y - node2.y)**2)
+
+parser = ArgumentParser()
+parser.add_argument("--input", "-i", help="input data")
+parser.add_argument("--max", help="maximal dinstance for one truck")
+args = parser.parse_args()
+
+process_input_data(args.input, args.max)
